6482f0e1cde6e71d9512014a1c56ef45fce8e3cf
Category <tuankietcoder2211@gmail.com>
2024-12-03 22:57:45 +0700

    create

---
+++ src/main/java/websocket_chat_oauth2/websocket_chat_oauth2/WebsocketChatOauth2Application.java
@@ -0,0 +1,13 @@
+package websocket_chat_oauth2.websocket_chat_oauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketChatOauth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsocketChatOauth2Application.class, args);
+	}
+
+}
