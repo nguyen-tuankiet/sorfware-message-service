0c4ce52815ece16ebdade1a3f4480a0b5c5d89d0
kiet-nlu <tuankietcoder2211@gmail.com>
2025-03-23 23:04:55 +0700

    create dir

---
+++ backend/src/test/java/websocket_chat_oauth2/websocket_chat_oauth2/WebsocketChatOauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package websocket_chat_oauth2.websocket_chat_oauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketChatOauth2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
