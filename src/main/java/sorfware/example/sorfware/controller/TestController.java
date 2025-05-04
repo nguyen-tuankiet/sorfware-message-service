3f4d15ffd8f23196422039fe8176426dd028c96b
22130136@st.hcmuaf.edu.vn <144929114+nguyen-tuankiet@users.noreply.github.com>
2025-05-04 22:40:27 +0700

    Update TestController.java

--- src/main/java/sorfware/example/sorfware/controller/TestController.java
+++ src/main/java/sorfware/example/sorfware/controller/TestController.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 @RequestMapping("")
