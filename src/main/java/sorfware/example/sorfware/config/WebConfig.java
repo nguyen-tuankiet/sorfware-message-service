a44676ee267e1495dcc519920eaf2f3817d9e01c
22130136@st.hcmuaf.edu.vn <144929114+nguyen-tuankiet@users.noreply.github.com>
2025-05-20 23:22:50 +0700

    Update WebConfig.java

--- src/main/java/sorfware/example/sorfware/config/WebConfig.java
+++ src/main/java/sorfware/example/sorfware/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://chat-realtime-fe.onrender.com")
+                        .allowedOrigins("https://chat-realtime-fe.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
