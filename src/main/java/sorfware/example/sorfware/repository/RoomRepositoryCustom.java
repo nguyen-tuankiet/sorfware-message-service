9f68a1157ebf8173a77f6dd73d63064b938d1ac9
Võ Nguyễn Thanh Diệu <22130039@st.hcmuaf.edu.vn>
2025-05-24 22:47:54 +0700

    fix error in room reponsitory custom

--- src/main/java/sorfware/example/sorfware/repository/RoomRepositoryCustom.java
+++ src/main/java/sorfware/example/sorfware/repository/RoomRepositoryCustom.java
@@ -1,10 +1,11 @@
 package sorfware.example.sorfware.repository;
 
+import org.springframework.data.mongodb.repository.MongoRepository;
 import sorfware.example.sorfware.model.entity.Room;
 
 import java.util.List;
 
-public interface RoomRepositoryCustom {
+public interface RoomRepositoryCustom{
     /**
      * UC3.1
      * Hiển thị danh sách cuộc trò chuyện theo id user và sắp xếp theo thời gian tin nhắn cuối cùng*/
